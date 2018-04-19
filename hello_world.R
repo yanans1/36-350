0a200fde082dbddc3dfa798aedc975e832f7ba02
yanans1 <yanans1.andrew.cmu.edu>
2018-04-19 19:20:30 -0400

    create a model select

--- hello_world.R
+++ hello_world.R
@@ -5,3 +5,15 @@ generate_data=function(n,p){
   responses=rnorm(n)
   return (list(covariates=covariates,responses=responses))
 }
+
+model_select=function(covariates, responses,cutoff){
+  res=lm(responses ~ covariates)
+  res.sum=summary(res)
+  p.val=res.sum$coefficients[2:(ncol(covariates)+1),"Pr(>|t|)"]
+  valid_cov=covariates[,p.val<=cutoff]
+  if(length(valid_cov)==0) return(c())
+  new=lm(responses~valid_cov)
+  new.sum=summary(new)
+  new.pval=new.sum$coefficients[,"Pr(>|t|)"]
+  return(new.pval)
+}
